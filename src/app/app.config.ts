c8ec4c9f1bd5ccb4e2528c9f399581cee012e0ca
Murilo Rodrigues <murilorodrigues@MacBook-Pro-de-Murilo.local>
2024-01-28 14:43:06 -0300

    feat(i18n): add i18n, material and tailwind to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTransloco } from '@ngneat/transloco';
+import {TranslocoHttpLoader} from "./shared/services/transloco.service";
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideHttpClient(), provideTransloco({
+        config: {
+            availableLangs: ['en', 'pt'],
+            defaultLang: 'pt',
+            reRenderOnLangChange: true,
+            prodMode: !isDevMode(),
+        },
+        loader: TranslocoHttpLoader
+    }), provideAnimations()]
 };
---
+++ src/app/shared/services/transloco.service.ts
@@ -0,0 +1,17 @@
+import {inject, Injectable} from "@angular/core";
+import {Translation, TranslocoLoader} from "@ngneat/transloco";
+import {TranslocoLoaderData} from "@ngneat/transloco/lib/transloco.loader";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TranslocoHttpLoader implements TranslocoLoader {
+  private http = inject(HttpClient);
+
+  getTranslation(lang: string) {
+    return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
+  }
+
+}
